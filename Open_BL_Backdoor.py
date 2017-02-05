ef31144d2f787aa26b2156a4e300d49a89dfce9e
Jorgenhoyer <hoyerjorgen@gmail.com>
2017-02-05 15:55:23 +0100

    Update Open_BL_Backdoor.py

--- Open_BL_Backdoor.py
+++ Open_BL_Backdoor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ##Open cc2650 bootloader backdoor, via Beaglebone Green, by Jorgen Hoyer##
-#Basert paa ccfg innstillinger fra JelmerT sin guide om flashing av image på cc13xx/cc26xx via USB
+#Basert paa ccfg innstillinger fra JelmerT sin guide om flashing av image paa cc13xx/cc26xx via USB
 
 import Adafruit_BBIO.GPIO as GPIO #import GPIO Library
 
