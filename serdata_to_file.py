58e0719eec17021391bcf006205cc437cd132063
Jorgenhoyer <hoyerjorgen@gmail.com>
2017-02-14 12:21:53 +0100

    Update serdata_to_file.py

--- serdata_to_file.py
+++ serdata_to_file.py
@@ -10,9 +10,7 @@ import serial
 import time
 f = open('ser_logg.txt', 'w')
 ser = serial.Serial("/dev/ttyACM0", baudrate=115200, stopbits=1, parity="N", timeout=1)
-outPin_rst="P9_12"                #set outPin for  Reset to "P9_12"
 
-GPIO.setup(outPin_rst,GPIO.OUT)   #make outPin_rst an Output
 
 while True:
     tid = strftime("%Y-%m-%d %H:%M:%S")
